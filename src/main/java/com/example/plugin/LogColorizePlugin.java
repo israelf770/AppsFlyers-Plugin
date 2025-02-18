69f7c213efc5ff0fa0eaca8f23fa0b5a71681d6e
Meir-Patakum <meirhersonski@gmail.com>
2025-02-18 17:40:13 +0200

    change func HW to be bar, not log

--- src/main/java/com/example/plugin/LogColorizePlugin.java
+++ src/main/java/com/example/plugin/LogColorizePlugin.java
@@ -3,15 +3,12 @@ package com.example.plugin;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 
 public class LogColorizePlugin extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        Project project = e.getProject();
-        if (project != null) {
-            // כאן נדפיס את "Hello World" למסך
-            System.out.println("Hello World");
-        }
+        Messages.showMessageDialog("Hello world!", "Test Title", Messages.getInformationIcon()  );
     }
 }
